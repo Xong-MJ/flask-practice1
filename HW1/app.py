21a9ea234174d689e7b089b43beba7a79dabede7
SongMinjun <smj007869@gmail.com>
2026-09-23 18:54:20 +0900

    / 페이지: 이름과 학번 추가

--- HW1/app.py
+++ HW1/app.py
@@ -2,5 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return render_template("index.html", name="송민준", student_id="24013813")
+
 if __name__ == "__main__":
     app.run(debug=True)
