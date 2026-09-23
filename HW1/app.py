ab6bf7152377af2f84bda68afdc41c5f9c3530e5
SongMinjun <smj007869@gmail.com>
2026-09-23 18:55:50 +0900

    /profile 페이지: 취미 목록 추가

--- HW1/app.py
+++ HW1/app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html", name="송민준", student_id="24013813")
 
+@app.route("/profile")
+def profile():
+    hobbies = ["춤추기", "노래부르기", "게임하기"]
+    return render_template("profile.html", hobbies=hobbies)
+
 if __name__ == "__main__":
     app.run(debug=True)
